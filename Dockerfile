94e83425c8b2a6a50504fa686a456ccffbf6f57d
lbh666 <1050208869@qq.com>
2023-11-15 21:18:43 +0800

    update

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM registry.iaticetc.cn:5000/lbh_nerf:latest
+
+COPY alexnet-owt-7be5be79.pth /root/.cache/torch/hub/checkpoints/
--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 OUTDIR='/mnt/share/lbh/'
 DATADIR='/mnt/datasets'
 
-python train.py --output_root=${OUTDIR}/Ec-NeRF_wkdir --data.root=${OUTDIR}/event_nope_nerf/data/blender \
- --
+mkdir -p /run/determined/workdir/.cache/torch/hub/checkpoints
+cp ${OUTDIR}/ckpts/alexnet-owt-7be5be79.pth /run/determined/workdir/.cache/torch/hub/checkpoints/alexnet-owt-7be5be79.pth
+python train.py --output_root=${OUTDIR}/Ev-NeRF_wkdir --data.root=${OUTDIR}/event_nope_nerf/data/blender \
+ --group=NeRF --name=no_downsample --visdom=false \
+ --model=nerf --yaml=nerf_blender_repr --data.image_size=[800,800]
